ac08d080da2d6bcead4bd7a69eb389a163e68a83
thealgor <sk4.wizkid@gmail.com>
2018-11-12 12:02:03 -0800

    Update RotatedBinarySearch.java

--- medium/BinarySearch/RotatedBinarySearch.java
+++ medium/BinarySearch/RotatedBinarySearch.java
@@ -1,6 +1,30 @@
 package BinarySearch;
 
 class RotatedBinarySearch {
+    
+    public int search(int[] nums, int target) {
+          if(nums==null || nums.length==0)
+            return -1;
+        int lo=0, hi=nums.length-1;
+        while(lo<hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>=nums[lo]){
+                if(target>=nums[lo]&&target<nums[mid]) hi=mid-1;
+                else lo=mid+1;
+                
+            }
+            else{
+                if(target>nums[mid]&&target<=nums[hi]) lo=mid+1;
+                else hi=mid-1;
+            }
+        }
+       return nums[lo] == target ? lo : -1;
+    }
+    
+    
+    
+    
     public int search(int[] nums, int target) {
 
         int low = 0, high = nums.length-1, n = nums.length;
