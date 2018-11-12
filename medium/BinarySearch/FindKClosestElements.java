135c800a6b42d193fefe0423d53df6e91040c37c
thealgor <sk4.wizkid@gmail.com>
2018-11-12 15:09:54 -0800

    Update FindKClosestElements.java

--- medium/BinarySearch/FindKClosestElements.java
+++ medium/BinarySearch/FindKClosestElements.java
@@ -1,6 +1,27 @@
-//look for better solution.
-
+//BetterSolution
 class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+         int lo = 0, hi = arr.length - k;
+         while (lo < hi) {
+         int mid = (lo + hi) / 2;
+         if (x - arr[mid] > arr[mid+k] - x)
+            lo = mid + 1;
+         else
+            hi = mid;
+     }
+        
+     List<Integer> result = new ArrayList();
+     for(int i=lo;i<lo+k;i++) 
+        result.add(arr[i]);
+     return result;
+    }
+}
+
+
+
+
+
+class Solution1{
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int res = findClosestElement(arr,x);
         List<Integer> solution = findTheResult(arr, res, k, x);
