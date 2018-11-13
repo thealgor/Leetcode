8fc46dcfd2663469d2ef885409c4481b4d6d0ac1
thealgor <sk4.wizkid@gmail.com>
2018-11-12 23:29:36 -0800

    Update FirstAndLastPositionInArray.java

--- medium/BinarySearch/FirstAndLastPositionInArray.java
+++ medium/BinarySearch/FirstAndLastPositionInArray.java
@@ -14,7 +14,48 @@ package medium.BinarySearch;
 *
 * duplicates allowed or not, general question to ask for every array problem.
 */
-class FirstAndLastPositionInArray {
+
+//better solution
+class Solution {
+    // returns leftmost (or rightmost) index at which `target` should be
+    // inserted in sorted array `nums` via binary search.
+    private int extremeInsertionIndex(int[] nums, int target, boolean left) {
+        int lo = 0;
+        int hi = nums.length;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > target || (left && target == nums[mid])) {
+                hi = mid;
+            }
+            else {
+                lo = mid+1;
+            }
+        }
+
+        return lo;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] targetRange = {-1, -1};
+
+        int leftIdx = extremeInsertionIndex(nums, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if (leftIdx == nums.length || nums[leftIdx] != target) {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = extremeInsertionIndex(nums, target, false)-1;
+
+        return targetRange;
+    }
+}
+
+
+class FirstAndLastPositionInArray1 {
     public int[] searchRange(int[] nums, int target) {
         int l = 0, r=nums.length-1;
         int res1=-1, res2=-1;
